7f3000370c20518cae006d6b1139999fb3a93eb3
Jeff Morhous <morhous.jeff@gmail.com>
2023-02-13 22:38:55 -0500

    Make a database query for states

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,54 @@
 var express = require('express');
 var router = express.Router();
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize(process.env.CONNECTION_URI, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+});
+
+const State = sequelize.define('State', {
+  // Model attributes are defined here
+  state_name: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    unique: false
+  },
+  capital: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    unique: false
+  },
+  state_bird: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    unique: false
+  },
+}, {
+  // Other model options go here
+});
+
+async function connectToDB() {
+  try {
+    sequelize.authenticate().then(async () => {
+      // await State.sync({ alter: true });
+    })
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+connectToDB();
+
+/* GET a new resource */
+router.get('/state', async function(req, res, next) {
+  const state = await State.findByPk(req.query.id);
+  if (state) {
+    res.send(state)
+  } else {
+    res.status(404).send("state not found");
+  }
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
